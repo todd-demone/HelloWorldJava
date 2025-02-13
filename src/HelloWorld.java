89cb94a606175e0b0ecab15e31f2027fab05d10a
Todd Demone <tdemone1@gmail.com>
2025-02-13 12:44:18 -0500

    Take user input from console

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
 //        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int first = Integer.parseInt(scanner.next());
+        System.out.println("Enter a second number: ");
+        int second = Integer.parseInt(scanner.next());
         HelloWorld obj = new HelloWorld();
-        int result = obj.sum(4, 4);
-        System.out.printf("The result is %d%n", result);
+        int result = obj.sum(first, second);
+        System.out.printf("The sum of %d and %d is %d%n", first, second, result);
         System.out.printf("I have a string named %s, an integer of %d and something this much cash: $%.2f%n", "\"MyString\"", 42, 10.13);
     }
 
