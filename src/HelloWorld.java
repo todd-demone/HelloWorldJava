596a858718fecb646823a83cc60c03791ecf0a30
Todd Demone <tdemone1@gmail.com>
2025-02-13 12:08:45 -0500

    String formatting

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,8 @@ public class HelloWorld {
 //        System.out.println("Hello, World!");
         HelloWorld obj = new HelloWorld();
         int result = obj.sum(4, 4);
-        System.out.println(result);
+        System.out.printf("The result is %d%n", result);
+        System.out.printf("I have a string named %s, an integer of %d and something this much cash: $%.2f%n", "\"MyString\"", 42, 10.13);
     }
 
     public int sum(int first, int second) {
