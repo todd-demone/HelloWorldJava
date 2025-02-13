653018b73fe288b363d2bf681dd2e6d4e31d048e
Todd Demone <tdemone1@gmail.com>
2025-02-13 11:58:55 -0500

    Add sum method

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+//        System.out.println("Hello, World!");
+        HelloWorld obj = new HelloWorld();
+        int result = obj.sum(4, 4);
+        System.out.println(result);
+    }
+
+    public int sum(int first, int second) {
+        return 42;
     }
 }
